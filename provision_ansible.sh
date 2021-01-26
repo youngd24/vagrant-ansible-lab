1faab7cdbc511fba2fb38c7979f1e677f0cd3ae0
TellTaleMajora <juanjose1092@gmail.com>
2021-01-26 17:30:38 -0600

    deploys ansible 2.9 with python3

--- provision_ansible.sh
+++ provision_ansible.sh
@@ -3,11 +3,7 @@ echo "Starting provisioning of Ansible..."
 
 sudo apt-get update
 sudo apt-get install git -y
-sudo apt-get install python-setuptools -y
-sudo apt-get install python-dev -y
-sudo easy_install pip
-sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update -y
-sudo apt-get install ansible -y
-
+sudo apt-get install python3-pip -y
+pip3 install ansible
+echo "export PATH=$PATH:$HOME/.local/bin" >> /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
